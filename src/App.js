535e3c23f70a52ddb6353555edfe0e47c7db154f
pro8tick <madengineerps@gmail.com>
2023-07-28 13:00:30 +0530

    dot env added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './app.css'
-import Header from './Components/Header';
-import { makeStyles } from '@material-ui/core';
-import Alert  from './Components/Alert'
-import { lazy,Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./app.css";
+import Header from "./Components/Header";
+import { makeStyles } from "@material-ui/core";
+import Alert from "./Components/Alert";
+import { lazy, Suspense } from "react";
 
 const Home = lazy(() => import("./Pages/Home"));
 const Coins = lazy(() => import("./Pages/Coins"));
 
-
 function App() {
-  const useStyle = makeStyles(()=>({
-    App:{
-      backgroundColor:"#14161a",
-      color:"white",
-      minHeight:"100vh"
-    }
-  }))
-
+  const useStyle = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+  console.log(process.env);
   const classes = useStyle();
 
   return (
     <>
-        
       <BrowserRouter>
-        <div className={classes.App}>                    
-            <Header/>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                  <Route path ='/' element={<Home/>}/>
-                  <Route path ='/coins/:id' element={<Coins/>}/>
-              </Routes>      
-            </Suspense>
+        <div className={classes.App}>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/coins/:id" element={<Coins />} />
+            </Routes>
+          </Suspense>
         </div>
-        <Alert/>
-      </BrowserRouter>      
-  
+        <Alert />
+      </BrowserRouter>
     </>
-   
   );
 }
 
--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,10 +1,19 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyB6z0B23kfTqSTxR4N8u9sjSxuMASnMCP0",
-    authDomain: "crypto-hunter-15fb5.firebaseapp.com",
-    projectId: "crypto-hunter-15fb5",
-    storageBucket: "crypto-hunter-15fb5.appspot.com",
-    messagingSenderId: "294913528668",
-    appId: "1:294913528668:web:0df046742c5eff90ea2799"
-  };
-  
-  export default firebaseConfig
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_API_ID,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDING_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDING_ID,
+  appId: REACT_APP_API_ID,
+};
+
+export default firebaseConfig;
